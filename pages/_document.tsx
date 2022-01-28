5dc2ba86b3e71e519d377662f3ee126f73e282f7
ertyurk <ertyurk@gmail.com>
2022-01-29 01:47:27 +0300

    final touch for gradient

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function MyDocument() {
   return (
     <Html>
       <Head />
-      <body className="bg-gradient-to-r from-orange-300 to-rose-300">
+      <body className="bg-gradient-to-r from-pink-200 to-blue-300">
         <Main />
         <NextScript />
       </body>
